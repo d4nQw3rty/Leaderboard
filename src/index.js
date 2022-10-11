7073e0b5fffc530988640f947e384f55ce8051cd
d4nQw3rty <danlinmoy@gmail.com>
2022-10-11 18:20:54 -0300

    fix linter

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ const scoresList = document.getElementById('scoreList');
 scores.forEach((score) => {
   const li = document.createElement('li');
   li.className = 'score';
-  if(score.id % 2 === 0) {
-    li.classList.add('gray-bg');    
+  if (score.id % 2 === 0) {
+    li.classList.add('gray-bg');
   }
   li.innerHTML = ` ${score.name} : ${score.score} `;
   scoresList.appendChild(li);
